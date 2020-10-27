eff8e5eb986677d7297fa1f7c7d1bfdb9db4dabe
YumingChen <mathcym@outlook.com>
2020-10-27 20:10:41 +0800

    [yuming chen]: implement get all students and add student api.

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/GroupController.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.capability.gtb.entrancequiz.controller;
+
+import com.thoughtworks.capability.gtb.entrancequiz.dto.Student;
+import com.thoughtworks.capability.gtb.entrancequiz.service.GroupService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class GroupController {
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return ResponseEntity.ok(groupService.getAllStudents());
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity addStudent(@RequestBody Student student) {
+        groupService.add(student);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/dto/Student.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.capability.gtb.entrancequiz.dto;
+
+public class Student {
+    private Integer id;
+    private String name;
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
